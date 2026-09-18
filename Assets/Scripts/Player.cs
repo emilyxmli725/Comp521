1316a8c7151eda4ed4b230f87b8bfe5f4b14d6b8
Emily Li <emilyxmli725@gmail.com>
2026-09-18 18:29:01 -0400

    game completed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,33 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private Camera cam;
+    [SerializeField] private GameObject bulletSpawn; 
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private float bulletSpeed; 
     [SerializeField] private float sensitivity;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float jumpForce;
-
-    [SerializeField] private float movementSpeed; 
+    [SerializeField] private float movementSpeed;
+    public float groundCheckDistance = 2.2f;
+    [SerializeField] LayerMask groundLayer;
+    public int ammo;
+    public GameObject bullet; 
+    private bool _isShooting;
     //turn up down 
-    private float pitch = 0f;
+    private float _pitch = 0f;
     // turn left right 
-    private float yaw = 0f;
+    private float _yaw = 0f;
 
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Destroy(other.gameObject);
+            ammo++;
+        }
+        
+    }
+    
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -24,7 +41,15 @@ public class Player : MonoBehaviour
     private void Update()
     {
         CameraInput();
-        
+        MovePlayer();
+    }
+
+    public void Clear()
+    {
+        ammo = 0;
+    }
+    private void MovePlayer()
+    {
         var direction = Vector3.zero;
 
         if (Keyboard.current.wKey .isPressed)
@@ -43,22 +68,33 @@ public class Player : MonoBehaviour
         {
             direction +=  Vector3.right;
         }
-        
-        transform.position += Quaternion.Euler(0f,yaw,0f) * direction * (Time.deltaTime * movementSpeed);
+        if(Mouse.current.leftButton.wasPressedThisFrame && ammo > 0 && !bullet)
+        {
+            bullet = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bullet.GetComponent<Rigidbody>().linearVelocity = cam.transform.forward * bulletSpeed; 
+            ammo--;
+            
+        }
+        transform.position += Quaternion.Euler(0f,_yaw,0f) * direction * (Time.deltaTime * movementSpeed);
 
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && GroundCheck())
         {
             rb.AddForce(Vector3.up * jumpForce,  ForceMode.Impulse);
         }
     }
 
+    private bool GroundCheck()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer );
+    }
+    
     private void CameraInput()
     {
         Vector2 mouseMovement = sensitivity * Mouse.current.delta.ReadValue();
-        pitch -=  mouseMovement.y;
-        yaw += mouseMovement.x;
+        _pitch -=  mouseMovement.y;
+        _yaw += mouseMovement.x;
         
-        pitch = Mathf.Clamp(pitch, -90f, 90f);
-        cam.transform.rotation = Quaternion.Euler(pitch,yaw, 0f);
+        _pitch = Mathf.Clamp(_pitch, -90f, 90f);
+        cam.transform.rotation = Quaternion.Euler(_pitch,_yaw, 0f);
     }
 }
